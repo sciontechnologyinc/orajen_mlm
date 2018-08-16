910aa9c07f6c175572da21d8e37260873738a8dd
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-08-17 02:50:49 +0800

    revised - admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('marketingPlan.pages.homePage');
+        return view('admin.index');
     }
 }
