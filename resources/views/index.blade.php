c6df4e52c97473db67da72e4d731cc89e41d63c0
Jetro Macalipay <wajjfreelance@gmail.com>
2018-08-21 02:33:19 +0800

    test
    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -117,12 +117,7 @@
         <div class="content-wrapper">
           <div class="row purchace-popup">
             <div class="col-12">
-              <span class="d-flex alifn-items-center">
-                <p>Like what you see? Check out our premium version for more.</p>
-                <a href="https://github.com/BootstrapDash/StarAdmin-Free-Bootstrap-Admin-Template" target="_blank" class="btn ml-auto download-button">Download Free Version</a>
-                <a href="https://www.bootstrapdash.com/product/star-admin-pro/" target="_blank" class="btn purchase-button">Upgrade To Pro</a>
-                <i class="mdi mdi-close popup-dismiss"></i>
-              </span>
+    
             </div>
           </div>
           <div class="row">
