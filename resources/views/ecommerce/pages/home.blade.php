943a3439ca1f58adc5ab99a0575f2d32e14f207a
Jetro Macalipay <wajjfreelance@gmail.com>
2018-08-26 04:08:28 +0800

    ecom

--- resources/views/ecommerce/pages/home.blade.php
+++ resources/views/ecommerce/pages/home.blade.php
@@ -10,6 +10,29 @@
         <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
       </header>
 
+<div class="featured-products">
+  Featured Products
+  <div class="row featured-product">
+    <div class="col-sm-4 featured-products">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="fp-label">ORAJEN Whitening Soap</div>
+    </div>
+    <div class="col-sm-4 featured-products">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="fp-label">ORAJEN Perfume & Cologne</div>
+    </div>
+    <div class="col-sm-4 featured-products">
+        <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+        <div class="fp-label">ORAJEN Supplements</div>
+    </div>
+  </div>
+</div>
+
+<div class="cover-home">
+  <div class="coverhome-label">
+      ORAJEN Premium Cologne
+  </div>
+</div>
 
 </div>
 @endsection
