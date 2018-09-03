7c50729d3a26d4c32dcd6bd77f25d6c11a326d7e
WAJJ FREELANCE <wajjfreelance@gmail.com>
2018-09-03 01:55:14 -0700

    sa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
                     You are logged in!
                 </div>
-                <example-component></example-component>
+                
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('newmember', function () {
 });
 
 Route::get('dashboard', function () {
-    return view('admin.index');
+    return view('dashboard.index');
 });
 
 Route::get('statistics', function () {
@@ -174,3 +174,21 @@ Route::get('account', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::post('/user', [
+    'uses' => 'LoginController@login',
+    'as'   => 'user.index'
+]);
+
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('user.index', function(){
+          return view('user.index');
+    })->name('user');;
+
+    Route::get('dashboard.index', function(){
+        return view('dashboard.index');
+    })->name('dashboard');
+
+});
