818848da850c283ed110e7e96f916f853392704c
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-09-05 04:13:00 +0800

    push

--- resources/views/ecommerce/pages/aboutus.blade.php
+++ resources/views/ecommerce/pages/aboutus.blade.php
@@ -8,11 +8,16 @@
 <div class="container">
         <div class="row">
             <div class="mission">
+            <p class="company-mission">Company Background</p>
+            ORAJEN is founded by a group of people who wants to build a strong global direct selling. They studied their experiences in different businesses and developed a platform with the best marketing plan. They created a business opportunity with premium quality products that everyone would be benefited. They committed to the distributors that no one will be left behind. ORA is a unit of money and JEN is a compassionate love for humanity. The founder believes that the combination of ORA and JEN will achieve their mission and vision.
+            </div>
+        </div>
+        <div class="row">
+            <div class="mission">
             <p class="company-mission">MISSION</p>
             To create a group of successful people that will help others to achieve their goals. To provide business opportunities to OFW so they will no longer need to work to other countries and be with their families.
             </div>
         </div>
-        <div class="row"></div>
         <div class="row">
             <div class="vision">
             <p class="company-vision">VISION</p>
--- resources/views/ecommerce/pages/home.blade.php
+++ resources/views/ecommerce/pages/home.blade.php
@@ -17,31 +17,75 @@
       </header>
 
 <div class="featured-products">
-  Featured Products
+  Packages
   <div class="row featured-product">
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/1.jpg" alt="">
-        <div class="fp-label">ORAJEN Placenta Organic Soap</div>
+    <div class="card-deck">
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="/images/packages/package_1.jpg" alt="Package 1">
+                <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title package">Package 1</h4>
+                <p class="card-text package">1 Bottle of Perfume<br> 1 pc Guava Soap<br> 2 packs 350 Real Detergent Powder<br> 2 pcs sachet 50g Real Detergent Powder(Free)</p>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="/images/packages/package_2.jpg" alt="Package 2">
+                <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title package">Package 2</h4>
+                <p class="card-text package">1 Set Perfume Tester<br> 1 pc Tawas Soap<br> 3 packs 350g Real Detergent Powder<br> 3 pcs sachet 50g Real Detergent Powder<br> 1 pc Papaya Soap(Free)</p>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="/images/packages/package_3.jpg" alt="Package 3">
+                <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title package">Package 3</h4>
+                <p class="card-text package">1 bot of Perfume 50ml for Men<br> 1 bot of Perfume 35ml for Women<br> 1 pc Glutamansi Soap<br> 1 pack 350g Real Detergent Powder<br> 2 pcs sachet 50g Real Detergnet Powder(Free)</p>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/2.jpg" alt="">
-        <div class="fp-label">ORAJEN Guava Organic Soap</div>
-    </div>
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/3.jpg" alt="">
-        <div class="fp-label">ORAJEN Glutamansi Organic Soap</div>
-    </div>
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/4.jpg" alt="">
-        <div class="fp-label">ORAJEN Bleaching Organic Soap</div>
-    </div>
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/6.jpg" alt="">
-        <div class="fp-label">ORAJEN REAL Detergent Powder</div>
-    </div>
-    <div class="col-sm-4 featured-products">
-        <img class="card-img-top" src="images/products/5.jpg" alt="">
-        <div class="fp-label">ORAJEN Tawas Organic Soap</div>
+  </div>
+  <div class="row featured-product">
+    <div class="card-deck">
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="/images/packages/package_4.jpg" alt="Package 4">
+                <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title package">Package 4</h4>
+                <p class="card-text package">2 bot of Perfume 35ml for Women<br> 1 pc Placenta Soap<br> 1 pack 350g Real Detergent Powder<br> 2 pcs sachet 50g Real Detergent Powder(Free)</p>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="/images/packages/package_5.jpg" alt="Package 5">
+                <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body">
+                <h4 class="card-title package">Package 5</h4>
+                <p class="card-text package">1 bot of Perfume 35ml for Women<br> 1 bot of Perfume 50ml for Men<br> 1 pc Bleaching Soap<br> 1 pack 350g Real Powder Detergent<br> 2 pcs sachet 50g Real Detergent Powder(Free)</p>
+            </div>
+        </div>
+        <div class="card mb-4">
+        </div>
     </div>
   </div>
 </div>
