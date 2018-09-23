0e5d8e3f16e524b25dc7f113c491bdf74abe7a71
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-09-24 05:26:02 +0800

    push

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,106 +3,16 @@
 @section('content')
 <div class="main-panel">
     <div class="content-wrapper">
-      <div class="row">
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-          <div class="card card-statistics">
-            <div class="card-body">
-              <div class="clearfix">
-                <div class="float-left">
-                  <i class="mdi mdi-cube text-danger icon-lg"></i>
-                </div>
-                <div class="float-right">
-                  <p class="mb-0 text-right">Total Revenue</p>
-                  <div class="fluid-container">
-                    <h3 class="font-weight-medium text-right mb-0">$65,650</h3>
-                  </div>
-                </div>
-              </div>
-              <p class="text-muted mt-3 mb-0">
-                <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-          <div class="card card-statistics">
-            <div class="card-body">
-              <div class="clearfix">
-                <div class="float-left">
-                  <i class="mdi mdi-receipt text-warning icon-lg"></i>
-                </div>
-                <div class="float-right">
-                  <p class="mb-0 text-right">Orders</p>
-                  <div class="fluid-container">
-                    <h3 class="font-weight-medium text-right mb-0">3455</h3>
-                  </div>
-                </div>
-              </div>
-              <p class="text-muted mt-3 mb-0">
-                <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-          <div class="card card-statistics">
-            <div class="card-body">
-              <div class="clearfix">
-                <div class="float-left">
-                  <i class="mdi mdi-poll-box text-success icon-lg"></i>
-                </div>
-                <div class="float-right">
-                  <p class="mb-0 text-right">Sales</p>
-                  <div class="fluid-container">
-                    <h3 class="font-weight-medium text-right mb-0">5693</h3>
-                  </div>
-                </div>
-              </div>
-              <p class="text-muted mt-3 mb-0">
-                <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-          <div class="card card-statistics">
-            <div class="card-body">
-              <div class="clearfix">
-                <div class="float-left">
-                  <i class="mdi mdi-account-location text-info icon-lg"></i>
-                </div>
-                <div class="float-right">
-                  <p class="mb-0 text-right">Employees</p>
-                  <div class="fluid-container">
-                    <h3 class="font-weight-medium text-right mb-0">246</h3>
-                  </div>
-                </div>
-              </div>
-              <p class="text-muted mt-3 mb-0">
-                <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Product-wise sales
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-7 grid-margin stretch-card">
-      
-        </div>
-        
-      </div>
+ 
+    <div class="um-img">
+    <img src="/images/undermaintenance-img.png" alt="">
     </div>
-    <!-- content-wrapper ends -->
-    <!-- partial:partials/_footer.html -->
-    <footer class="footer">
-      <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
-          <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
-          <i class="mdi mdi-heart text-danger"></i>
-        </span>
-      </div>
-    </footer>
-    <!-- partial -->
-  </div>
+
+   <div class="um-img">PAGE UNDER CONSTRUCTION</div>
+
+
+    <div class="company-logo"><img src="/img/logo.png" alt=""></div>
+    <div class="page-desc">Our website is coming soon. </div>
+    <div class="page-desc1">"This page will show admin dashboard."</div>
+    
 @endsection
