5bea2df4e0ae08b58abfe19c8bca894e61a64840
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-09-27 20:33:31 +0800

    push

--- public/js/ecommerce.js
+++ public/js/ecommerce.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     $('#navbar-header').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
+
 });
--- resources/views/ecommerce/pages/home.blade.php
+++ resources/views/ecommerce/pages/home.blade.php
@@ -1,21 +1,64 @@
 @extends('ecommerce.master.template')
 
 @section('content')
-<div class="cover-video">
-<video autoplay controls id="myVideo">
-  <source src="/videos/Cover_Video.MP4" type="video/mp4">
-</video>
+
+<div class="homecover">
+  <div class="cover-overlay">
+  <div class="padding"></div>
+    <div class="container">
+      <div class="row">
+          <div class="col col-md-12 col-lg-10 col-xl-88">
+              <div class="full-width-left"> 
+                <h4 class="makeiteasy">Make it easy with</h4>
+                  <h1 class="orajen">
+                    <span class="orajen-txt">ORAJEN</span><span class="ecommerce-txt"> eCommerce</span>
+                  </h1>
+                  <span class="cover-desc">A best place to create, discover and share opportunities.</span>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
 </div>
 
+<div class="container">
+  <div class="gallery">
+  <h2 class="pnr">PROMOTIONS & REWARDS</h2>
+   <section class="customer-logos slider">
+      <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+   </section>
+   </div>
+</div>
+
+<div class="section-video">
+  <div class="sv-overlay">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm sv1">
+          <h6 class="sv-predesc">A MUST-HAVE OF EVERY HOUSEHOLD</h6>
+          <h2 class="sv-maindesc">WHAT CAN YOU DO WITH ORAJEN eCOMMERCE</h2>
+          <p class="sv-postdesc">Pay your bills, buy load, game credits, sattelite subscriptions, etc and shop online - do it with rewards</p>
+        </div>
+        <div class="col-sm sv2">
+        <video autoplay controls id="myVideo">
+          <source src="/videos/Cover_Video.MP4" type="video/mp4">
+        </video>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="container">
-      <!-- Jumbotron Header -->
-      <header class="jumbotron my-4">
-        <h1 class="display-3" id="scroll-to">Welcome to ORAJEN ECOMMERCE!</h1>
-        <p class="lead"></p>
-        <a href="#" class="btn btn-primary btn-lg joinNow">Join Now !</a>
-      </header>
-
+  <!-- Gallery -->
+      <!-- Gallery -->
 <div class="featured-products">
   Featured Products
   <div class="row product-list">
@@ -237,18 +280,30 @@
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 
 <script>
 
-$(window).scroll(function() {
-   var hT = $('#scroll-to').offset().top,
-       hH = $('#scroll-to').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-   if (wS > (hT+hH-wH)){
-    $('#myVideo').trigger('pause');
-   }
-});
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
 
 </script>
 @endsection
