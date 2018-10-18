a7c98087d6d890a5fc0e209634676a90824a8823
wajjfreelance <39485182+wajjfreelance@users.noreply.github.com>
2018-10-19 00:24:20 +0800

    dd

--- resources/views/managepayouts/payoutlist.blade.php
+++ resources/views/managepayouts/payoutlist.blade.php
@@ -94,8 +94,21 @@
                           </button>
                           </div>
                           </td>
+
+                        </tr>
+                      </tbody>
+                      @endforeach
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+    </div>
+</div>
+
+
 <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
+<div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
 
           <!-- Modal content-->
@@ -114,20 +127,4 @@
     </div>
   </div>
  
-                        </tr>
-                      </tbody>
-                      @endforeach
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-    </div>
-</div>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
 @endsection
